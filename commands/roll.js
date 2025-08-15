39624030782d500c86db1755a7d90c7ce99fb090
owomed <owomedsunucu@gmail.com>
2025-08-15 10:58:36 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -3,20 +3,25 @@ module.exports = {
     description: 'Belirli kanalda Rollvs Katılımcısı rolünü etiketler.',
     async execute(client, message, args) {
         // Komutun kullanılabileceği kanalların ID'lerini ve kullanıcı ID'sini belirtin
-        const allowedChannelID = ['1373685143394320525'];
+        const allowedChannelIDs = ['1373685143394320525'];
         const allowedRoleID = '1238576058119487539';
         const mentionRoleID = '1238181107741098115';
 
         // Komutun kullanıldığı kanal ve kullanıcı kontrolü
-        if (!allowedChannelID.includes(message.channel.id)) {
-            return message.reply('`Bu komut bu kanalda kullanılamaz.`');
+        if (!allowedChannelIDs.includes(message.channel.id)) {
+            return message.reply({ content: '`Bu komut bu kanalda kullanılamaz.`' });
         }
 
         if (!message.member || !message.member.roles.cache.has(allowedRoleID)) {
-            return message.reply('`Bu komutu kullanma yetkiniz bulunmamaktadır.`');
+            return message.reply({ content: '`Bu komutu kullanma yetkiniz bulunmamaktadır.`' });
         }
 
         // Etiketlenecek rolü etiketleyen mesaj gönderin
-        message.channel.send(`<@&${mentionRoleID}>`);
+        try {
+            await message.channel.send(`<@&${mentionRoleID}>`);
+        } catch (error) {
+            console.error('Rol etiketlenirken bir hata oluştu:', error);
+            message.reply({ content: 'Rol etiketlenirken bir hata meydana geldi.' });
+        }
     },
 };
