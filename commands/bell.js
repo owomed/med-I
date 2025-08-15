6ec17e9bddcccd72fbb983d4825624ffeb777e7d
owomed <owomedsunucu@gmail.com>
2025-08-15 10:21:07 +0300

    Update bell.js

--- commands/bell.js
+++ commands/bell.js
@@ -4,32 +4,43 @@ module.exports = {
   args: true,
   usage: '@kullanıcı',
   async execute(client, message, args) {
-    if (!message.mentions.users.size) {
-      return message.reply('`Bir kullanıcı etiketlemelisin.`');
+    // Mesaj içeriği "args" ile gelirken, botun çalışması için
+    // öncelikle mesaj içeriğinin varlığına dair bir kontrol yapılması iyi olur.
+    if (!args[0]) {
+      return message.reply({ content: '`Bir kullanıcı etiketlemelisin.`' });
     }
 
-    const roleToCheck = '1247597263300198421'; // Kullanıcının sahip olup olmadığını kontrol edeceğimiz rol ID'si
-    const mentionedUser = message.mentions.members.first(); // Etiketlenen kullanıcı
-    const guild = message.guild; // Mesajın atıldığı sunucu
-
+    const roleToCheck = '1247597263300198421';
+    const mentionedUser = message.mentions.members.first();
+    
+    // Etiketlenen kullanıcının varlığını kontrol edin.
     if (!mentionedUser) {
-      return message.reply('`Belirtilen kullanıcı bulunamadı.`');
+      return message.reply({ content: '`Belirtilen kullanıcı bulunamadı.`' });
     }
 
+    const guild = message.guild;
     const role = guild.roles.cache.get(roleToCheck);
 
     if (!role) {
-      return message.reply(`\`ID'si ${roleToCheck} olan rol bulunamadı.\``);
+      return message.reply({ content: `\`ID'si ${roleToCheck} olan rol bulunamadı.\`` });
     }
 
     if (mentionedUser.roles.cache.has(roleToCheck)) {
-      // Kullanıcının rolü varsa al
-      await mentionedUser.roles.remove(roleToCheck);
-      return message.reply(`\`${mentionedUser.user.tag} adlı kullanıcıdan ${role.name} rolü alındı.\``);
+      try {
+        await mentionedUser.roles.remove(role);
+        return message.reply({ content: `\`${mentionedUser.user.tag} adlı kullanıcıdan ${role.name} rolü alındı.\`` });
+      } catch (error) {
+        console.error('Rol alma hatası:', error);
+        return message.reply({ content: 'Rol alınırken bir hata oluştu.' });
+      }
     } else {
-      // Kullanıcının rolü yoksa ver
-      await mentionedUser.roles.add(roleToCheck);
-      return message.reply(`\`${mentionedUser.user.tag} adlı kullanıcıya ${role.name} rolü verildi.\``);
+      try {
+        await mentionedUser.roles.add(role);
+        return message.reply({ content: `\`${mentionedUser.user.tag} adlı kullanıcıya ${role.name} rolü verildi.\`` });
+      } catch (error) {
+        console.error('Rol verme hatası:', error);
+        return message.reply({ content: 'Rol verilirken bir hata oluştu.' });
+      }
     }
   },
 };
