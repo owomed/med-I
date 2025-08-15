0f64c68985bd2f795efa78dc7f9393b9ab084d1e
owomed <owomedsunucu@gmail.com>
2025-08-15 11:13:37 +0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,44 +1,44 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder, MessageCollector } = require('discord.js');
 
 module.exports = {
   name: "ticket",
   description: "Kullanıcıya Ticket katılım rolü verir/alır.",
   args: true,
-  usage: "@kullanıcı veya 'all'", // Kullanım bilgisini güncelledim
+  usage: "@kullanıcı veya 'all'",
   async execute(client, message, args) {
     const allowedRoleID = "1238598132745506856";
     const roleToCheck = "1405559110677823622";
     const guild = message.guild;
     const logChannelID = "1237313546354823218";
     const logChannel = guild.channels.cache.get(logChannelID);
-    
+
     // Yetki kontrolü
     if (!message.member || !message.member.roles.cache.has(allowedRoleID)) {
-      return message.reply("`Bu komutu kullanma yetkiniz bulunmamaktadır.`");
+      return message.reply({ content: "`Bu komutu kullanma yetkiniz bulunmamaktadır.`" });
     }
 
     const role = guild.roles.cache.get(roleToCheck);
 
     if (!role) {
-      return message.reply(`\`ID'si ${roleToCheck} olan rol bulunamadı.\``);
+      return message.reply({ content: `\`ID'si ${roleToCheck} olan rol bulunamadı.\`` });
     }
-    
+
     // Toplu rol alma işlevi
     if (args[0] === 'all') {
       const membersWithRole = guild.members.cache.filter(member => member.roles.cache.has(roleToCheck));
-      
+
       // Onay mesajı gönder
-      const confirmationEmbed = new MessageEmbed()
-        .setColor('ORANGE')
+      const confirmationEmbed = new EmbedBuilder()
+        .setColor('Orange')
         .setDescription(`**${membersWithRole.size}** kişiden ${role} rolünü kaldırmak istediğinizden emin misiniz? Onaylamak için **evet** yazın.`);
-      
+
       const sentMessage = await message.channel.send({ embeds: [confirmationEmbed] });
-      
+
       // Kullanıcının onayını bekle
       const filter = response => response.author.id === message.author.id && response.content.toLowerCase() === 'evet';
       const collector = message.channel.createMessageCollector({ filter, time: 15000, max: 1 });
-      
-      collector.on('collect', async response => {
+
+      collector.on('collect', async collected => {
         let rolesRemovedCount = 0;
         for (const member of membersWithRole.values()) {
           try {
@@ -48,60 +48,70 @@ module.exports = {
             console.error(`Rol kaldırılamadı: ${member.user.tag}, Hata: ${error.message}`);
           }
         }
-        
-        const successEmbed = new MessageEmbed()
-          .setColor('GREEN')
+
+        const successEmbed = new EmbedBuilder()
+          .setColor('Green')
           .setDescription(`Başarıyla **${rolesRemovedCount}** kişiden ${role} rolü kaldırıldı. <a:med_verify3:1242796325121298532>`);
-        message.channel.send({ embeds: [successEmbed] });
-        
+        await message.channel.send({ embeds: [successEmbed] });
+
         if (logChannel) {
-          const logEmbed = new MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL())
-            .setColor('GREEN')
+          const logEmbed = new EmbedBuilder()
+            .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
+            .setColor('Green')
             .setDescription(`**${message.author.tag}** tarafından **${rolesRemovedCount}** kişiden ${role} rolü toplu olarak kaldırıldı.`);
-          logChannel.send({ embeds: [logEmbed] });
+          await logChannel.send({ embeds: [logEmbed] });
         }
-        sentMessage.delete().catch(() => {});
-        response.delete().catch(() => {});
+        await sentMessage.delete().catch(() => {});
+        await collected.first().delete().catch(() => {});
       });
-      
+
       collector.on('end', collected => {
         if (collected.size === 0) {
-          const timeoutEmbed = new MessageEmbed()
-            .setColor('RED')
+          const timeoutEmbed = new EmbedBuilder()
+            .setColor('Red')
             .setDescription('Onay süresi doldu. Toplu rol kaldırma işlemi iptal edildi.');
           message.channel.send({ embeds: [timeoutEmbed] });
         }
       });
-      
+
       return;
     }
 
     // Normal rol ekleme/kaldırma işlevi
     if (!message.mentions.users.size) {
-      return message.reply("Bir kullanıcı etiketlemelisin.");
+      return message.reply({ content: "`Bir kullanıcı etiketlemelisin.`" });
     }
-    
+
     const mentionedUser = message.mentions.members.first();
-    let embed = new MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
       .setColor('Random');
 
     if (mentionedUser.roles.cache.has(roleToCheck)) {
       // Kullanıcının rolü varsa al
-      await mentionedUser.roles.remove(roleToCheck);
-      embed.setDescription(`${mentionedUser} adlı kullanıcıdan ${role} rolü alındı. <a:med_verify3:1242796325121298532>`);
-      message.channel.send({ embeds: [embed] });
-      if (logChannel) {
-        logChannel.send({ embeds: [embed] });
+      try {
+        await mentionedUser.roles.remove(roleToCheck);
+        embed.setDescription(`${mentionedUser} adlı kullanıcıdan ${role} rolü alındı. <a:med_verify3:1242796325121298532>`);
+        await message.channel.send({ embeds: [embed] });
+        if (logChannel) {
+          await logChannel.send({ embeds: [embed] });
+        }
+      } catch (error) {
+        console.error(`Rol kaldırma hatası: ${error}`);
+        message.reply({ content: 'Rol kaldırılırken bir hata oluştu.' });
       }
     } else {
       // Kullanıcının rolü yoksa ver
-      await mentionedUser.roles.add(roleToCheck);
-      embed.setDescription(`${mentionedUser} adlı kullanıcıya ${role} rolü verildi. <a:med_verify3:1242796325121298532>`);
-      message.channel.send({ embeds: [embed] });
-      if (logChannel) {
-        logChannel.send({ embeds: [embed] });
+      try {
+        await mentionedUser.roles.add(roleToCheck);
+        embed.setDescription(`${mentionedUser} adlı kullanıcıya ${role} rolü verildi. <a:med_verify3:1242796325121298532>`);
+        await message.channel.send({ embeds: [embed] });
+        if (logChannel) {
+          await logChannel.send({ embeds: [embed] });
+        }
+      } catch (error) {
+        console.error(`Rol verme hatası: ${error}`);
+        message.reply({ content: 'Rol verilirken bir hata oluştu.' });
       }
     }
   },
